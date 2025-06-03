b6ec8f2c77ef30976241bfc6ba649fcd963e3507
gcavalleri <gcavalleri@fi.uba.ar>
2025-06-03 04:04:38 -0300

    new changes for the unit, player and test

--- src/main/test/edu/fiuba/algo3/entrega_1/TestCase03.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/TestCase03.java
@@ -39,13 +39,65 @@ public class TestCase03 {
         player = game.getCurrentPlayer();
         player.distributeCards(mazo.getCards());
 
-        player.playCard(game.getBoard());
-        player.playCard(game.getBoard());
-        player.playCard(game.getBoard());
-        player.playCard(game.getBoard());
-        player.playCard(game.getBoard());
+        player.playCard(game.getBoard(), 0);
+        player.playCard(game.getBoard(),1);
+        player.playCard(game.getBoard(),2);
+        player.playCard(game.getBoard(),3);
+        player.playCard(game.getBoard(),4);
+        //tene en cuenta que se borran las cartas una vez usadas de la mano y la siguiente toma su posicion
 
-        Assert.assertEquals(player.getScore(), 16);
+        Assert.assertEquals(player.getScore(), 17);
 
+    }
+    @Test
+    void TestCase06CartasUnidasduplicanValor() {
+
+        Game game = new Game();
+        Deck mazo = new Deck();
+
+        Unit Catapult = new Unit("Catapult", 8, siege, "Duplica si hay otra carta igual", new TightBond());
+        mazo.addCard(Catapult);
+        mazo.addCard(Catapult);
+        mazo.addCard(Catapult);
+        Unit Cow = new Unit("Cow", 0, range, "");
+        mazo.addCard(Cow);
+        mazo.addCard(Cow);
+        Unit GeraltOfRivia = new Unit("Geralt of Rivia", 15, melee,"Superman");
+        mazo.addCard(GeraltOfRivia);
+        Unit Roach = new Unit("Roach", 4, melee, "");
+        mazo.addCard(Roach);
+        Unit Vesemir = new Unit("Catapult", 6, melee, "viejo fuerte");
+        mazo.addCard(Vesemir);
+        Unit Arachas = new Unit("Arachas", 4, melee, "");
+        Unit Cockatrice = new Unit("Cockatrice", 4, melee, "Un cocodrilo que busca su gemelo", new TightBond());
+        mazo.addCard(Cockatrice);
+        mazo.addCard(Cockatrice);
+
+        game.setPlayerRound();
+        Player player = game.getCurrentPlayer();
+        player.distributeCards(mazo.getCards());
+        int total = player.getCardHand();
+
+        for (int i = 0; i < total; i++) {
+            player.playCard(game.getBoard(), 0);
+        }
+
+        Assert.assertEquals(player.getScore(), 89);
+    }
+    @Test
+    void TestCase06addTrheeCatapultAndTheEffectActivate() {
+        Player player1 = new Player("Player1");
+        Player player2 = new Player("Player2");
+        Board board = new Board(player1, player2);
+
+        Unit catapult1 = new Unit("Catapult", 8, siege, "Duplica si hay otra carta igual", new TightBond());
+        Unit catapult2 = new Unit("Catapult", 8, siege, "Duplica si hay otra carta igual", new TightBond());
+        Unit catapult3 = new Unit("Catapult", 8, siege, "Duplica si hay otra carta igual", new TightBond());
+
+        catapult1.play(board, player1);
+        catapult2.play(board, player1);
+        catapult3.play(board, player1);
+
+        Assert.assertEquals(board.scorePlayer(player1),48);
     }
 }
