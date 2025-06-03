b41fabc4cd44471beaeba135032426a29a889040
gcavalleri <gcavalleri@fi.uba.ar>
2025-06-03 04:04:38 -0300

    new changes for the unit, player and test

--- src/main/java/edu/fiuba/algo3/clases/Specials.java
+++ src/main/java/edu/fiuba/algo3/clases/Specials.java
@@ -4,7 +4,13 @@ public class Specials extends Cards implements Effect {
 
     public Specials(String name, String description) {
 
-        super(name, description);
-    }
+        super(name, description, true);
 
+    }
+    public void effect(Board board, Player player, Unit Self) {
+
+    }
+    public void play(Board board, Player player) {
+
+    }
 }
