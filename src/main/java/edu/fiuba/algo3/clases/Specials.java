c351da1cdb288a4a3f057e099de23d283338f4b3
gcavalleri <gcavalleri@fi.uba.ar>
2025-06-02 23:15:54 -0300

    test03 add card in the board and add score value

---
+++ src/main/java/edu/fiuba/algo3/clases/Specials.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3.clases;
+
+public class Specials extends Cards implements Effect {
+
+    public Specials(String name, String description) {
+
+        super(name, description);
+    }
+
+    public void effect() {
+    }
+}
